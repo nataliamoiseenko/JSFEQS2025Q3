9bf097e02fdb1e436a3a7faef6a8f4e285b19c4e
nataliamoiseenko <natallia.moiseenko@gmail.com>
2025-10-19 17:50:32 +0200

    feat: configure eslint

--- coffee-house/src/scripts/burgerMenu.ts
+++ coffee-house/src/scripts/burgerMenu.ts
@@ -2,12 +2,14 @@ const bmToggle = document.getElementById('burger-menu-toggle')!;
 const bmLinks = document.getElementsByClassName('header__burger-menu-list-link burger-link');
 
 bmToggle.addEventListener('change', (e) => {
-  (e.currentTarget as HTMLInputElement).checked
-    ? document.body.classList.add('no-scroll')
-    : document.body.classList.remove('no-scroll');
+  if ((e.currentTarget as HTMLInputElement).checked) {
+    document.body.classList.add('no-scroll');
+  } else {
+    document.body.classList.remove('no-scroll');
+  }
 });
 
-for (let link of bmLinks) {
+for (const link of bmLinks) {
   link.addEventListener('click', () => {
     (bmToggle as HTMLInputElement).checked = false;
     document.body.classList.remove('no-scroll');
